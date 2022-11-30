f906753de29184f65d2fb7fc9556d3415bc44596
Sila Siangchin <siangchin_s@silpakorn.edu>
2022-12-01 00:12:49 +0700

    first commoit webhhook and discord-bigbot

---
+++ bot/bot.go
@@ -0,0 +1,44 @@
+package bot
+
+import(
+	"fmt"
+	"github.com/silabig1294/discord-bigbot/config"
+	"github.com/bwmarrin/discordgo"
+)
+
+var BotID string
+var goBot *discordgo.Session
+
+
+func Start(){
+	goBot,err := discordgo.New("Bot "+config.Token)
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	u , err := goBot.User("@me")
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	BotID = u.ID
+	goBot.AddHandler(messageHandler)
+
+	err = goBot.Open()
+	if err != nil {
+		fmt.Println(err.Error())
+		return	
+	}
+	fmt.Println("Bot is running!")
+}
+
+func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate){
+	if m.Author.ID == BotID{
+		return 
+	}
+
+	if m.Content == "hello"{
+		_,_ = s.ChannelMessageSend(m.ChannelID,"Hello People")
+	}
+}
---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import(
+	"fmt"
+	"github.com/silabig1294/discord-bigbot/bot"
+	"github.com/silabig1294/discord-bigbot/config"
+)
+
+func main()  {
+	err := config.ReadConfig()
+
+	if err != nil{
+		fmt.Println(err.Error())
+		return
+	}
+
+	bot.Start()
+
+	<-make(chan struct{})
+	return
+}
